56554bde44ef548788b1f2a1be88d6bc0c0885c3
Claire McLeod <clmcleod@Claires-MacBook-Air.local>
2015-09-24 12:28:05 -0400

    file path structure mac and windows compatible; updated syntax to python 3.5
    via use of os.path.join()
    syntax updates to move from python 2.7 to 3.5

--- blindImages/blindImages.py
+++ blindImages/blindImages.py
@@ -1,5 +1,5 @@
 #blindImages.py: renames and reorganizes images for blinded image analysis.
-#Claire McLeod, cm.mcleod@gmail.com, last updated 2015-05-05
+#Claire McLeod, cm.mcleod@gmail.com, last updated 2015-09-23
 #
 #Currently supports .tif/.tiff, .png, and .jpg; you can add more file extensions in line 36
 #When run, will move all images in 'basePath' to two subfolders: blindedImages and originalImages.
@@ -26,10 +26,10 @@ Nmax=500 #maximum number used as blinded ID
 
 #don't change anything below here (except for different file extensions if needed)
 os.chdir(basePath)
-print "Blinding Files in: " + basePath
-blindPath=basePath + '\\blindedImages\\'
-origPath=basePath + '\\originalImages\\'
-blindingNotesPath=basePath + '\\blindingNotes\\'
+print ("Blinding Files in: " + basePath)
+blindPath=os.path.join(basePath, 'blindedImages')
+origPath=os.path.join(basePath, 'originalImages')
+blindingNotesPath=os.path.join(basePath, 'blindingNotes')
 nameList=[]
 
 #create a list of all of the images in basePath	
@@ -60,28 +60,28 @@ def moveFile(src, dest):
 
 #establish or locate the list of numbers to assign       
 if os.path.exists(blindingNotesPath):
-    print 'Reading existing blinding notes.'
+    print ('Reading existing blinding notes.')
     
     #read in existing blindingNotes
-    numAssignments=np.loadtxt(blindingNotesPath+'assignList.txt')
-    nameList=np.ndarray.tolist(np.genfromtxt(blindingNotesPath+'listNames.csv', delimiter=None, dtype=None))
+    numAssignments=np.loadtxt( os.path.bind(blindingNotesPath, 'assignList.txt'))
+    nameList=np.ndarray.tolist(np.genfromtxt( os.path.bind(blindingNotesPath, 'listNames.csv'), delimiter=None, dtype=None))
     assignIndex=len(nameList)
-    print 'Begining blinding from index:'
-    print assignIndex
+    print ('Begining blinding from index:')
+    print (assignIndex)
     
 if not os.path.exists(blindingNotesPath):
-    print 'Initializing folders.'
+    print ('Initializing folders.')
     os.makedirs(blindingNotesPath)
     #create list of numbers to assign
     numAssignments=np.random.choice(range(Nmin,Nmax), Nmax-Nmin, replace=False)
-    np.savetxt(blindingNotesPath+'assignList.txt', numAssignments)
+    np.savetxt(os.path.join(blindingNotesPath, 'assignList.txt') , numAssignments)
     assignIndex=0
 		
 #rename/relocate images
 for imgName in fileList:
     
     randNum=numAssignments[assignIndex]
-    print assignIndex
+    print (assignIndex)
     blindID=str(int(randNum)).zfill(3)
     assignIndex=assignIndex+1
     
@@ -93,12 +93,12 @@ for imgName in fileList:
 
     #copy the original image to blindedImages folder (blinded name)
     fileName, fileExtension = os.path.splitext(imgName)
-    blindName=blindPath + blindID + fileExtension
+    blindName=os.path.join(blindPath, blindID + fileExtension)
     copyFile(imgName, blindName)
     nameList.append(imgName)
 
     #move the original image to separate folder (name unchanged)      
-    moveName=origPath + imgName
+    moveName=os.path.join(origPath, imgName)
     moveFile(imgName, moveName)
     
 #update blindingNotes with exported csvs
@@ -106,14 +106,14 @@ a=np.asarray(nameList)
 filesMatched=np.vstack((a,numAssignments[0:assignIndex])).T
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
 
-with open(blindingNotesPath+'matchedFilenames.csv', 'wb') as csvfile:
+with open( os.path.join(blindingNotesPath, 'matchedFilenames.csv') , 'w',newline='') as csvfile:
     listwriter=csv.writer(csvfile, delimiter=',')
     for item in filesMatched:
         listwriter.writerow(item)
-
-with open(blindingNotesPath+'listNames.csv', 'wb') as csvfile:
+ 
+with open(os.path.join(blindingNotesPath, 'listNames.csv'), 'w', newline='') as csvfile:
     listwriter=csv.writer(csvfile, delimiter=',')
     for item in nameList:
         listwriter.writerow([item])
 
-print "Done."
+print ("Done.")
